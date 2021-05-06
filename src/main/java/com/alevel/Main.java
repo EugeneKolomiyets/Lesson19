33374c8eac0c49ba2dd24e70f36f7b695bd4036d
EugeneKolomiyets <evgenum@gmail.com>
2021-05-07 00:55:35 +0300

    start

---
+++ src/main/java/com/alevel/Main.java
@@ -0,0 +1,12 @@
+package com.alevel;
+
+public class Main {
+    public static void main(String[] args) {
+        //ctrl+Alt+V создать переменную после написания функции
+        //ctrl+alt+L форматирование
+        //ctrl+alt+M вынести кусок в отдлеьный метод
+        //ctrl+f+12 вызывает список методов класса
+        //ctrl+shift+/ коммент текста в средине
+        //ctrl+/ коммент построчно
+    }
+}
